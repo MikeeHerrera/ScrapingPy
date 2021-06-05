8bffcb692e27baf571fc4f93bf3fc9d9b02759a4
MikeeHerrera <laloaburto62@gmail.com>
2021-06-05 12:14:48 -0500

    imprimiendo datos de bayer con expresiones regulares

--- project/main.py
+++ project/main.py
@@ -1,5 +1,5 @@
 import requests
-
+import re
 url = 'http://econpy.pythonanywhere.com/ex/001.html'
 
 if __name__ == '__main__': 
@@ -8,8 +8,28 @@ if __name__ == '__main__':
 
     if response.status_code == 200:
         content = response.text #str
-        print(content)
+        # print(content)
 
-     
-    # with open('econpy.html','w+') as file:
-    #     file.write(content)
+        # modulo 1 imprimiendo datos de bayer manera 1
+        regexa = '<div title="buyer-name">'
+        regexb = '</div>'
+
+    for line in content.split('\n'):
+
+        if regexa in line:
+            start = line.find(regexa) + len (regexa)
+
+            end = line.find(regexb)
+            title = line[start:end]
+
+            print(title)
+    with open('econpy.html','w+') as file:
+        file.write(content)
+
+
+    # manera 2//
+
+    regex = '<div title="buyer-name">(.+?)</div>'
+
+    for title in re.findall(regex, content):
+        print(title)
